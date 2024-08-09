9c40b3b48bb0166a2c850cbf5c48e2caac26ce71
Catalina Rivarola <catyribe20@gmail.com>
2024-08-09 15:48:44 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+
+peso = float(input("Introduce tu peso: "))
+
+altura = float(input("Introduce tu altura: "))
+
+imc = peso / (altura * altura)
+
+print("Tu IMC es:", imc)
---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+#Escribir un programa que lea dos palabras y las compare. ¿Por qué
+#del resultado?
+palabra1 = input("Ingresa la primera palabra: ")
+palabra2 = input("Ingresa la segunda palabra: ")
+
+print(palabra1 == palabra2)
