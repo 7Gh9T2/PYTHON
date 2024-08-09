5469569e145304aee6ecf49991fc325d858504de
Catalina Rivarola <catyribe20@gmail.com>
2024-08-09 16:20:37 -0400

    Add files via upload

---
+++ Dia2/ejercicio1.py
@@ -0,0 +1,19 @@
+#esto es un comentario
+# la funcion print() se usa para imprimir texto
+print ("Bienvenido al curso de PROGRAMACION PHYTHON" )
+
+"""
+Este es un comentario en varias lineas usando 
+TRES comillas dobles.
+LOS COMENTARIOS NO ALTERAN EL PROGRAMA
+"""
+nombre = input ("¿Cual es tu nombre: ?")
+print("Un gusto conocerte", nombre, "!")
+
+print(" Vaya tu nombre tiene" , len (nombre), "letras !")
+
+
+
+
+
+
---
+++ Dia2/ejercicio3.py
@@ -0,0 +1,34 @@
+print("la suma de 10 + 4 es:", 3+4)
+#resolver la ecuacion cuadratica: 2 x** - 7 x+3 = 0
+a = 2
+b = -7
+c = 3
+
+print ( "SNPP" + "CTFPPJ" + "PROGRAMACION PYTHON")
+#print ("AULA" + 2102)
+
+#operadores mixtas
+print("Tun chi" *5)
+print ("Ja"* (2**3))
+
+# operadores de comparacion
+print(3>4)
+print(3<4)
+print(3>=4)
+print(4<=4)
+print(3==4)
+print(3!=4)
+
+#operaciones con cadenas de texto
+
+print("python"> "PYTHON")
+print("aaa">="abaa")
+print(len("aaa")>=len("abaa"))
+
+print("python"!= "PYTHON")
+
+### operadores logicos
+
+print(10>4 and "Z"> "A")
+print(10>4 or "Z"> "A")
+print(not(10>4) and "Z">"A")
